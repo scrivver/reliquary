ab75882fb1c7d0e55e0c0fe7dad2a77ec3117585
ChunHou20c <chunhouthatbornin20c@gmail.com>
2026-03-15 00:17:05 +0800

    nix: update dependency in flutter

--- shells/frontend.nix
+++ shells/frontend.nix
@@ -3,7 +3,11 @@
 pkgs.mkShell {
   name = "reliquary-frontend-shell";
   inputsFrom = [ infraShell ];
+  nativeBuildInputs = [
+    pkgs.pkg-config
+  ];
   buildInputs = [
     pkgs.flutter
+    pkgs.libsecret
   ];
 }
